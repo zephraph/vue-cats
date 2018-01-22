d635165fdb5f3f5a2e79051692612c11a0ab8b12
tessaSAC <tessaSAC@users.noreply.github.com>
2018-01-22 11:20:19 -0500

    fix merge conflict looool

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,53 @@
 <template>
-<<<<<<< HEAD
-  <div id="app">
-    <ViewSlider />
-=======
   <div class="App">
     <cat-container/>
+    <ViewSlider />
     <closet/>
->>>>>>> 5d0e1cb7200acb657cc072424cc780852ce0ae20
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import ViewSlider from './components/ViewSlider'
-=======
 import CatContainer from './components/CatContainer';
+import ViewSlider from './components/ViewSlider'
 import Closet from './components/Closet';
->>>>>>> 5d0e1cb7200acb657cc072424cc780852ce0ae20
 
 export default {
   name: 'app',
   components: {
-<<<<<<< HEAD
-    ViewSlider
+    CatContainer,
+    ViewSlider,
+    Closet
   },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-=======
-    CatContainer,
-    Closet
->>>>>>> 5d0e1cb7200acb657cc072424cc780852ce0ae20
   }
 }
 </script>
 
-<style>
-<<<<<<< HEAD
+<style scoped>
 #app {
   margin: 0 auto;
+  /* might need to take both of these out later idk */
+  display: flex;
+  padding: 10px;
 }
-=======
-  html, body, .App {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
-</style>
-
-<style scoped>
-  .App {
-    display: flex;
-    padding: 10px;
-  }
-  .CatContainer, .Closet {
-    border: 1px dashed black;
-  }
-  .CatContainer {
-    width: calc(75% - 20px);
-    margin-right: 10px;
-  }
-  .Closet {
-    width: calc(25% - 10px);
-  }
->>>>>>> 5d0e1cb7200acb657cc072424cc780852ce0ae20
+html, body, .App {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+.CatContainer, .Closet {
+  border: 1px dashed black;
+}
+.CatContainer {
+  width: calc(75% - 20px);
+  margin-right: 10px;
+}
+.Closet {
+  width: calc(25% - 10px);
+}
 </style>
 
