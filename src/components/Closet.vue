3c1b453c00bf5566596f4cee3a5b5c5891997d4e
tessaSAC <tessaSAC@users.noreply.github.com>
2018-01-24 21:09:58 -0500

    start work on drag

--- src/components/Closet.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="Closet">
-
-  </div>
-</template>
-
-<style scoped>
-</style>
-
---
+++ src/components/TheCloset.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="TheCloset">
+    <div v-for="(accessory, idx) in accessories" :key="idx" :style="setHeight(accessory)" class="accessory-box">
+      <ViewAccessory :imgUrl="accessory.imgUrl"></ViewAccessory>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import ViewAccessory from './ViewAccessory'
+
+export default {
+  components: {
+    ViewAccessory,
+  },
+  data() {
+    return {
+      accessories: [
+        {
+          imgUrl: 'https://www.placecage.com/100/100',
+          // width: 100,
+          height: 100,
+        }, {
+          imgUrl: 'https://www.placecage.com/100/100',
+          // width: 100,
+          height: 100,
+        }, {
+          imgUrl: 'https://www.placecage.com/100/100',
+          // width: 100,
+          height: 100,
+        },
+      ]
+    }
+  },
+  methods: {
+    setHeight(accessory) {
+      return `height: ${ accessory.height }px`
+    }
+  }
+}
+</script>
+
+<style>
+.TheCloset {
+  overflow: scroll;
+  padding: 5px;
+  text-align: center;
+}
+.accessory-box {
+  /* display: inline-block; */
+  margin: 5px;
+}
+</style>
+
