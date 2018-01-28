95cdb9edce5b2061a470f8354e1ec704770c8999
tessaSAC <tessaSAC@users.noreply.github.com>
2018-01-28 15:18:25 -0500

    Adding cat container component

---
+++ src/components/TheCatContainer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="CatContainer">
+    <!-- <slot /> -->
+    <ViewSlider v-for="(part, idx) in catParts" :key="idx" :images="part" />
+  </div>
+</template>
+
+<script>
+import ViewSlider from './ViewSlider'
+
+export default {
+  components: {
+    ViewSlider
+  },
+  data() {
+    return {
+      catParts: [
+        [
+          'https://www.placecage.com/100/100',
+          'src/assets/heads/01.png',
+          'src/assets/heads/02.png',
+        ], [
+          'https://www.placecage.com/100/100',
+          'src/assets/torsos/01.png',
+          'src/assets/torsos/02.png',
+        ], [
+          'https://www.placecage.com/100/100',
+          'src/assets/legs/01.png',
+          'src/assets/legs/02.png',
+        ],
+      ]
+    }
+  }
+}
+</script>
